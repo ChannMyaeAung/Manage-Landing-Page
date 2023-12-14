36b2c4229a90cdeab88e518e1b8f6f29699dcbc7
ChannMyaeAung <chanmyaeaung@my.jcu.edu.au>
2023-12-14 17:39:12 +0630

    implemented desktop versions of Services component.

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -26,9 +26,9 @@ const services = [
 
 const Services = () => {
   return (
-    <section id="" className="flex flex-col gap-10 mt-16">
+    <section id="" className="flex flex-col gap-10 mt-16 md:flex-row">
       {/* Service Info */}
-      <article className="flex flex-col items-center justify-center gap-3 px-4 text-center">
+      <article className="flex flex-col items-center justify-center gap-3 px-4 text-center md:items-start md:justify-start md:text-start">
         <h1 className="text-2xl font-bold">
           What's different <br className="block md:hidden" /> about Manage?
         </h1>
@@ -44,17 +44,17 @@ const Services = () => {
           return (
             <article
               key={service.id}
-              className="relative grid grid-cols-[1fr,1fr] px-4"
+              className="relative grid grid-cols-[62px,1fr] px-4 w-full "
             >
-              <h1 className="py-1.5 flex col-span-2 gap-3 items-center my-4 before:content-['0'] before:absolute before:bg-VeryPaleRed before:w-full  before:-z-10 before:py-1.5 before:rounded-l-full md:before:hidden">
-                <span className=" text-VeryLightGray font-bold text-sm py-1.5 rounded-full px-4 bg-BrightRed ">
+              <h1 className="py-1.5 flex md:grid md:grid-cols-[50px,1fr] col-span-2 gap-3 items-center my-4 md:my-0 before:content-['0'] before:absolute before:bg-VeryPaleRed before:w-full  before:-z-10 before:py-1.5 before:rounded-l-full md:before:hidden">
+                <span className=" text-VeryLightGray md:col-[1/2] font-bold text-sm py-1.5 rounded-full px-4 bg-BrightRed xl:px-1.5 xl:text-xl text-center">
                   0{service.id}
                 </span>
-                <span className="text-sm font-extrabold tracking-tight text-DarkBlue">
+                <span className="text-sm md:col-[2/-1] font-extrabold tracking-tight text-DarkBlue md:text-lg xl:text-2xl">
                   {service.title}
                 </span>
               </h1>
-              <p className="text-base col-[1/-1] text-DarkGrayishBlue">
+              <p className="text-base xl:text-lg col-[2/-1] text-DarkGrayishBlue">
                 {service.desc}
               </p>
             </article>
