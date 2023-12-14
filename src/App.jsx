882dc8186ca059c2e412364576668e1ab7c38f7d
ChannMyaeAung <chanmyaeaung@my.jcu.edu.au>
2023-12-14 16:02:56 +0630

    Updated the wrong component name of Hero component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
-import Hero from "./components/HEro";
+import Hero from "./components/Hero";
 import Services from "./components/Services";
 import Testimonials from "./components/Testimonials";
 import GetStarted from "./components/GetStarted";
